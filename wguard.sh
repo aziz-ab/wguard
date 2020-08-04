935da8f40f2aeb3ac445929ad42d8a744254772d
Aziz <abdul.aziz@muf.co.id>
2020-08-04 16:49:17 +0700

    Fix MTU script.

--- wguard.sh
+++ wguard.sh
@@ -80,6 +80,7 @@ function installQuestions() {
     read -rp "Server's WireGuard port: " -e -i "${SERVER_PORT}" SERVER_PORT
 
     SERVER_MTU = "1420"
+	read -rp "Server's MTU: " -e -i "${SERVER_MTU}" SERVER_MTU
 
 	# Adguard DNS by default
 	read -rp "First DNS resolver to use for the clients: " -e -i 1.1.1.1 CLIENT_DNS_1
